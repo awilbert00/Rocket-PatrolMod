07b654263ce92741ad283122e3276bb409daedce
Alexa Wilbert <awilbert@ucsc.edu>
2021-04-19 15:00:16 -0700

    added comments

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-//redesign theme/aesthetic - 60 (basically done)
-//create new spaceship type - 20 (done)  or add background music and speed increase after thirty seconds
-//display the time remaining - 10 (done)
-//implement scoring mechanism that
-//adds time for successful hits - 20 
+/*
+Alexa Wilbert
+Rocket Patrol Mod: Dragon Patrol
+4/19/2021
+~16 hours
+
+Sound Effects:
+Fireball Sound: https://freesound.org/people/CTCollab/sounds/223615/ 
+                - link to the license: https://creativecommons.org/licenses/by/3.0/legalcode
+Select Sound: https://freesound.org/people/kneekoo/sounds/548497/
+              - link to the license: https://creativecommons.org/licenses/by-nc/3.0/legalcode
+Explosion: https://freesound.org/people/misosound/sounds/251759/
+           - link to the license: https://creativecommons.org/publicdomain/zero/1.0/
+
+
+Points Breakdown: 
+Redesign theme/aesthetic - 60 (done)
+Create new spaceship type - 20 (done)  
+Display the time remaining - 10 (done)
+Implement scoring mechanism that
+adds time for successful hits - 20 (still working on this one)
+
+*/
+
 
 let config = {
     type: Phaser.CANVAS,
